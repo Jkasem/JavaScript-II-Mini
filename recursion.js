7a8d98ed8427637bbb5113d983b40009214eda3d
Justin Kaseman <justinkaseman@live.com>
2018-02-22 17:12:44 -0800

    Finish recursion & this.js word answers

--- recursion.js
+++ recursion.js
@@ -9,20 +9,20 @@ while (n <= 10) {
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
-// code here
+const countToTen = () => {
+  if (n > 10) return;
+  countToTen(++n);
+}
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen());
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
 const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
+  if (n < 2) return 1;
+  return n * nFactorial(n - 1);
 };
 
 console.log(factorial(5));
@@ -30,4 +30,4 @@ console.log(factorial(5));
 // write the above function in a recursive way.
 
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial());
--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global binding - points to the global scobe because it was invoked there
+* 2. Implicit Binding - this happens when there is a dot used in the function call AKA there is an object calling the function
+* 3. new binding - when constructor creates the object this refers to the new object getting created
+* 4. explicit binding - refers to when invoked with call or apply
 *
 * write out a code example of each explanation above
 */
@@ -13,16 +13,14 @@ console.log('hello world!');
 
 // Principle 1
 
-// code example for Window Binding
+
 
 // Principle 2
 
-// code example for Implicit Binding
+
 
 // Principle 3
 
-// code example for New Binding
+
 
 // Principle 4
-
-// code example for Explicit Binding
